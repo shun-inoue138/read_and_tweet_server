4548835087f6ef8f60d68304e34e913d28b52eda
inoue <ht8sk13@icloud.com>
2023-01-25 21:30:03 +0900

    タスク登録、一覧取得、ID別取得のルーティングを記述

--- src/v1/routes/tasks.js
+++ src/v1/routes/tasks.js
@@ -1,12 +1,30 @@
 const router = require("express").Router();
 const express = require("express");
+const { body } = require("express-validator");
+const { registerTask, getAllTasks, getTask } = require("../controllers/tasks");
+const { tasksValidator } = require("../handlers/validation");
 const app = express();
 
 app.use(express.json());
 require("dotenv").config();
 
-router.get("/", (req, res) => {
-  res.json("tasks router!");
-});
+//タスク登録
+router.post(
+  "/",
+  body("url").isURL().withMessage("URLを入力してください"),
+  body("title").isLength({ min: 1 }).withMessage("タイトルを入力してください"),
+  body("dueDate").isDate().withMessage("日付を入力してください"),
+  body("postContent")
+    .isLength({ max: 140 })
+    .withMessage("140文字以内で入力してください"),
+  tasksValidator,
+  registerTask
+);
+
+//タスク一覧取得
+router.get("/", getAllTasks);
+
+//タスク取得
+router.get("/:id", getTask);
 
 module.exports = router;
